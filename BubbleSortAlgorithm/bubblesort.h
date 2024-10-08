1897a079d34f47ffc6a83ad8d1c4c4d45f1e38d7
Chatchapong Thuaypha <chatchapong1@windowslive.com>
2024-10-08 16:59:53 -0700

    Update

--- BubbleSortAlgorithm/bubblesort.h
+++ BubbleSortAlgorithm/bubblesort.h
@@ -1,10 +1,12 @@
-#ifndef HEADER_H
-#define HEADER_H
+#ifndef BUBBLESORT_H
+#define BUBBLESORT_H
 
-// Function to perform bubble sort
-void bubbleSort(int arr[], int size);
+namespace Sort {
+    // Function to perform bubble sort
+    void bubbleSort(int arr[], int size);
 
-// Function to print the array
-void printArray(int arr[], int size);
+    // Function to print the array
+    void printArray(int arr[], int size);
+}
 
 #endif
--- BubbleSortAlgorithm/main.cpp
+++ BubbleSortAlgorithm/main.cpp
@@ -1,14 +1,19 @@
-//
-//  main.cpp
-//  BubbleSortAlgorithm
-//
-//  Created by Chatchapong Thuaypha on 10/7/24.
-//
-
 #include <iostream>
+#include "bubblesort.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main() {
+    int arr[] = {997, 584, 396, 145, 220, 486, 798};
+    int size = sizeof(arr) / sizeof(arr[0]);   // calculates the size of the array
+    
+    cout << "Original array: " << endl;
+    Sort::printArray(arr, size);  // Print the original array using Sort namespace
+
+    Sort::bubbleSort(arr, size);  // Call the bubble sort function using Sort namespace
+
+    cout << "Sorted array: " << endl;
+    Sort::printArray(arr, size);  // Print the sorted array using the Sort namespace
+
     return 0;
 }
